bfcc23f31b04bf63fc43296b15b8d7afeb0f70bc
OysteinKoder <oystein.kodehode@gmail.com>
2022-07-24 11:26:11 +0200

    added text through i18n

--- public/locales/en/en.js
+++ public/locales/en/en.js
@@ -1,3 +1,8 @@
 export default {
   test: "english",
+  landing_page_navbar_about: "About",
+  landing_page_navbar_frontend_course: "Course",
+  landing_page_header: "Frontend Course",
+  landing_page_navbar_selector_locale_en: "en",
+  landing_page_navbar_selector_locale_nor: "nor",
 };
